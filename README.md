5025459d29dbd6e2a7e0dcd4ce63b78c44652730
Magic Len <len@magiclen.org>
2021-09-18 10:48:58 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+韋諾之戰繁體中文翻譯
+==========
+
+## 如何使用翻譯檔？
+
+#### Linux
+
+將`mo`目錄中的所有`.mo`檔案覆蓋到`/usr/share/wesnoth/translations/zh_TW/LC_MESSAGES`目錄中即可。
+
+## 翻譯進度查詢
+
+https://www.wesnoth.org/gettext/?view=langs&version=branch&lang=zh_TW
+
+## 尚未完全翻譯的部份
+
+* wesnoth-utbs
+* wesnoth-manual
+* wesnoth-wc
+
+## 繁體中文化團隊
+
+http://wesnoth-tw.blogspot.com/
---
+++ po2mo.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# convert
+
+if [[ -d mo ]]; then
+    rm -rf mo/*
+else
+    mkdir mo
+fi
+
+if [[ -d po_upload ]]; then
+    rm -rf po_upload/*
+else
+    mkdir po_upload
+fi
+
+for po in ./po/*.po; do
+    filename=$(basename -- $po)
+    filename_noext=${filename%.*}
+    
+    # convert
+    msgfmt "$po" -o "./mo/${filename_noext}.mo"
+    
+    # rearrange
+    f="po_upload/${filename_noext}"
+    mkdir "${f}"
+    cp "${po}" "${f}/zh_TW.po"
+done
